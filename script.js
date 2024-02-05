74a663a29deada7da47283202ed2736c89e6c0bb
DavidRamalho86 <david.ramalho@aluno.senai.br>
2024-02-05 18:01:05 -0300

    iniciando uma calculadora de imc

---
+++ script.js
@@ -0,0 +1,11 @@
+function calculadoraIMC () {
+    const peso = parseFloat(document.getElementById ('peso').value);
+    const altura = parseFloat (document.getElementById ('altura').value);
+
+    const imc = peso / (altura*altura);
+    const resultadoCalculo = document.getElementById ('resultado');
+
+    if ( imc < 18,5) {
+        
+    }
+}
